9db71bf6665ef871b10efca9c006d4ac007ab7ae
didiermorantes <didiermorantes@gmail.com>
2019-07-26 16:31:52 -0500

    creacion de carpeta

---
+++ MVC_CRUD/Controllers/miTablaController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_CRUD.Models;
+using MVC_CRUD.Models.ViewModels;
+
+namespace MVC_CRUD.Controllers
+{
+    public class miTablaController : Controller
+    {
+        // GET: miTabla
+        public ActionResult Index()
+        {
+            List<ListTablaViewModel> lst;
+            using (miBaseDatosEntities2 db = new miBaseDatosEntities2())
+            {
+                lst=(from d in db.miTabla
+                        select new ListTablaViewModel
+                        {
+                            Id=d.id,
+                            Nombre=d.nombre,
+                            Correo = d.correo,
+                           
+                           
+                            
+                        }).ToList();
+            }
+            return View(lst);
+        }
+    }
+}
---
+++ MVC_CRUD/Models/ViewModels/ListTablaViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_CRUD.Models.ViewModels
+{
+    public class ListTablaViewModel
+    {
+        /*representa los elemento */
+        public string Id { get; set; }
+        public string Nombre { get; set; }
+        public string Correo { get; set; }
+        public DateTime Fecha { get; set; }
+
+    }
+}
